025ee3338067ee43c53b957656a90170e6893309
zhangwei <zw_pku@163.com>
2018-01-13 20:14:39 +0800

    20180113

--- full_conected_OO/full_connect_oo.py
+++ full_conected_OO/full_connect_oo.py
@@ -39,7 +39,7 @@ class Node(object):
         """
         self.output = output
 
-    def append_downstream_connection(self,conn):
+    def append_downstream_connection(self, conn):
         """
         添加一个到下游节点的连接
         :param conn: 连接
@@ -47,7 +47,7 @@ class Node(object):
         """
         self.downstream.append(conn)
 
-    def append_upstream_append(self,conn):
+    def append_upstream_connection(self, conn):
         """
         添加一个到上游节点的连接
         :param conn: 连接
@@ -106,7 +106,7 @@ class ConstNode(object):
         :param layer_index: 节点所属层的编号
         :param node_index: 节点的编号
         """
-        self.lay_index = layer_index
+        self.layer_index = layer_index
         self.node_index = node_index
         self.downstream = []
         self.output = 1
@@ -164,7 +164,7 @@ class Layer(object):
         :return: 
         """
         for i in range(len(data)):
-            self.nodes[i].set_ouput(data[i])
+            self.nodes[i].set_output(data[i])
 
     def calc_output(self):
         """
@@ -271,6 +271,7 @@ class Network(object):
             for conn in connections:
                 self.connections.add_connection(conn)
                 conn.downstream_node.append_upstream_connection(conn)
+                conn.upstream_node.append_downstream_connection(conn)
 
     def train(self, labels, data_set, rate, iteration):
         """
@@ -378,17 +379,100 @@ class Normalizer(object):
         return list(map(lambda m: 0.9 if number & m else 0.1, self.mask))
 
     def denorm(self, vec):
-        binary = map(lambda i: 1 if i > 0.5 else 0, vec)
+        binary = list(map(lambda i: 1 if i > 0.5 else 0, vec))
         for i in range(len(self.mask)):
             binary[i] = binary[i] * self.mask[i]
         return reduce(lambda x, y: x + y, binary)
 
-    def mean_square_error(vec1, vec2):
+
+def mean_square_error(vec1, vec2):
         return 0.5 * reduce(lambda a, b: a + b,
                             map(lambda v: (v[0] - v[1]) * (v[0] - v[1]),
                                 zip(vec1, vec2)
                                 )
                             )
-    
+
+
+def gradient_check(network, sample_feature, sample_label):
+        '''
+        梯度检查
+        network: 神经网络对象
+        sample_feature: 样本的特征
+        sample_label: 样本的标签
+        '''
+        # 计算网络误差
+        network_error = lambda vec1, vec2: \
+            0.5 * reduce(lambda a, b: a + b,
+                         map(lambda v: (v[0] - v[1]) * (v[0] - v[1]),
+                             zip(vec1, vec2)))
+
+        # 获取网络在当前样本下每个连接的梯度
+        network.get_gradient(sample_feature, sample_label)
+
+        # 对每个权重做梯度检查
+        for conn in network.connections.connections:
+            # 获取指定连接的梯度
+            actual_gradient = conn.get_gradient()
+
+            # 增加一个很小的值，计算网络的误差
+            epsilon = 0.0001
+            conn.weight += epsilon
+            error1 = network_error(network.predict(sample_feature), sample_label)
+
+            # 减去一个很小的值，计算网络的误差
+            conn.weight -= 2 * epsilon  # 刚才加过了一次，因此这里需要减去2倍
+            error2 = network_error(network.predict(sample_feature), sample_label)
+
+            # 根据式6计算期望的梯度值
+            expected_gradient = (error2 - error1) / (2 * epsilon)
+
+            # 打印
+            print('expected gradient: \t%f\nactual gradient: \t%f' % (
+                expected_gradient, actual_gradient))
+
+def train_data_set():
+    normalizer = Normalizer()
+    data_set = []
+    labels = []
+    for i in range(0, 256, 8):
+        n = normalizer.norm(int(random.uniform(0, 256)))
+        data_set.append(n)
+        labels.append(n)
+    return labels, data_set
+
+
+def train(network):
+    labels, data_set = train_data_set()
+    network.train(labels, data_set, 0.3, 50)
+
+
+def test(network, data):
+    normalizer = Normalizer()
+    norm_data = normalizer.norm(data)
+    predict_data = network.predict(norm_data)
+    print ('\ttestdata(%u)\tpredict(%u)' % (
+        data, normalizer.denorm(predict_data)))
+
+
+def correct_ratio(network):
+    normalizer = Normalizer()
+    correct = 0.0
+    for i in range(256):
+        if normalizer.denorm(network.predict(normalizer.norm(i))) == i:
+            correct += 1.0
+    print ('correct_ratio: %.2f%%' % (correct / 256 * 100))
+
+
+def gradient_check_test():
+    net = Network([2, 2, 2])
+    sample_feature = [0.9, 0.1]
+    sample_label = [0.9, 0.1]
+    gradient_check(net, sample_feature, sample_label)
+
+if __name__ == '__main__':
+    net = Network([8, 3, 8])
+    train(net)
+    net.dump()
+    correct_ratio(net)
 
 
