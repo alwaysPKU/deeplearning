5a376d53f5426baa4e2dfbe25658416ceb3f98d9
zhangwei <zw_pku@163.com>
2018-01-13 21:38:10 +0800

    修改bug，可运行

--- full_conected_OO/full_connect_oo.py
+++ full_conected_OO/full_connect_oo.py
@@ -279,7 +279,7 @@ class Network(object):
         :param labels: 数组，训练样本的标签。每个元素是一个样本的标签
         :param data_set: 二维数组，训练样本特征。每个元素是一个样本的特征。
         :param rate: 学习率
-        :param iteration: 
+        :param iteration: 迭代次数
         :return: 
         """
         for i in range(iteration):
@@ -386,49 +386,50 @@ class Normalizer(object):
 
 
 def mean_square_error(vec1, vec2):
-        return 0.5 * reduce(lambda a, b: a + b,
-                            map(lambda v: (v[0] - v[1]) * (v[0] - v[1]),
-                                zip(vec1, vec2)
-                                )
+    return 0.5 * reduce(lambda a, b: a + b,
+                        map(lambda v: (v[0] - v[1]) * (v[0] - v[1]),
+                            zip(vec1, vec2)
                             )
+                        )
 
 
 def gradient_check(network, sample_feature, sample_label):
-        '''
-        梯度检查
-        network: 神经网络对象
-        sample_feature: 样本的特征
-        sample_label: 样本的标签
-        '''
-        # 计算网络误差
-        network_error = lambda vec1, vec2: \
-            0.5 * reduce(lambda a, b: a + b,
-                         map(lambda v: (v[0] - v[1]) * (v[0] - v[1]),
-                             zip(vec1, vec2)))
+    '''
+    梯度检查
+    network: 神经网络对象
+    sample_feature: 样本的特征
+    sample_label: 样本的标签
+    '''
+    # 计算网络误差
+    network_error = lambda vec1, vec2: \
+        0.5 * reduce(lambda a, b: a + b,
+                     map(lambda v: (v[0] - v[1]) * (v[0] - v[1]),
+                         zip(vec1, vec2)))
 
-        # 获取网络在当前样本下每个连接的梯度
-        network.get_gradient(sample_feature, sample_label)
+    # 获取网络在当前样本下每个连接的梯度
+    network.get_gradient(sample_feature, sample_label)
 
-        # 对每个权重做梯度检查
-        for conn in network.connections.connections:
-            # 获取指定连接的梯度
-            actual_gradient = conn.get_gradient()
+    # 对每个权重做梯度检查
+    for conn in network.connections.connections:
+        # 获取指定连接的梯度
+        actual_gradient = conn.get_gradient()
 
-            # 增加一个很小的值，计算网络的误差
-            epsilon = 0.0001
-            conn.weight += epsilon
-            error1 = network_error(network.predict(sample_feature), sample_label)
+        # 增加一个很小的值，计算网络的误差
+        epsilon = 0.0001
+        conn.weight += epsilon
+        error1 = network_error(network.predict(sample_feature), sample_label)
 
-            # 减去一个很小的值，计算网络的误差
-            conn.weight -= 2 * epsilon  # 刚才加过了一次，因此这里需要减去2倍
-            error2 = network_error(network.predict(sample_feature), sample_label)
+        # 减去一个很小的值，计算网络的误差
+        conn.weight -= 2 * epsilon  # 刚才加过了一次，因此这里需要减去2倍
+        error2 = network_error(network.predict(sample_feature), sample_label)
 
-            # 根据式6计算期望的梯度值
-            expected_gradient = (error2 - error1) / (2 * epsilon)
+        # 根据式6计算期望的梯度值
+        expected_gradient = (error2 - error1) / (2 * epsilon)
 
-            # 打印
-            print('expected gradient: \t%f\nactual gradient: \t%f' % (
-                expected_gradient, actual_gradient))
+        # 打印
+        print('expected gradient: \t%f\nactual gradient: \t%f' % (
+            expected_gradient, actual_gradient))
+
 
 def train_data_set():
     normalizer = Normalizer()
@@ -450,7 +451,7 @@ def test(network, data):
     normalizer = Normalizer()
     norm_data = normalizer.norm(data)
     predict_data = network.predict(norm_data)
-    print ('\ttestdata(%u)\tpredict(%u)' % (
+    print('\ttestdata(%u)\tpredict(%u)' % (
         data, normalizer.denorm(predict_data)))
 
 
@@ -460,7 +461,7 @@ def correct_ratio(network):
     for i in range(256):
         if normalizer.denorm(network.predict(normalizer.norm(i))) == i:
             correct += 1.0
-    print ('correct_ratio: %.2f%%' % (correct / 256 * 100))
+    print('correct_ratio: %.2f%%' % (correct / 256 * 100))
 
 
 def gradient_check_test():
@@ -470,7 +471,7 @@ def gradient_check_test():
     gradient_check(net, sample_feature, sample_label)
 
 if __name__ == '__main__':
-    net = Network([8, 3, 8])
+    net = Network([8, 100, 8])
     train(net)
     net.dump()
     correct_ratio(net)
